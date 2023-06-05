4254182f0067280b108396fe2cf533ccdaa11f95
EmreSahiner <emre@journeyapp.com>
2023-06-05 11:48:09 +0300

    DB setup done

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/pizza.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    useNullAsDefault: true,
+  }
+
+};
