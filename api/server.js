ad8b1c3a04387e8c81e1ba881bae4b0da17bf17b
EmreSahiner <emre@journeyapp.com>
2023-06-05 11:54:44 +0300

    BE security setup Done

--- api/server.js
+++ api/server.js
@@ -1,8 +1,15 @@
 //1. importlar
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
 
 //2. global middleware'lar
+server.use(helmet());
+server.use(cors());
+server.use(morgan('dev'));
 server.use(express.json());
 
 
